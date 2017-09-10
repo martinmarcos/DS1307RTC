9dc5f9d2509341e371011266482702c52ca143ae
Martin Marcos <mmarcos.arg@gmail.com>
2017-09-10 03:57:54 -0300

    Port to use non-blocking I2C libray
    Using Wayne Truchsess' I2C library (Rev5). Compiles succesfully but
    still needs testing.

--- DS1307RTC.cpp
+++ DS1307RTC.cpp
@@ -20,26 +20,37 @@
   
   30 Dec 2009 - Initial release
   5 Sep 2011 updated for Arduino 1.0
+  10 Sep 2017 updated to work with Wayne Truchsess' I2C library (Rev5)
  */
 
+#define USE_I2C_LIBRARY
 
 #if defined (__AVR_ATtiny84__) || defined(__AVR_ATtiny85__) || (__AVR_ATtiny2313__)
 #include <TinyWireM.h>
 #define Wire TinyWireM
 #else
+#ifdef USE_I2C_LIBRARY
+#include <I2C.h>
+#else //USE_I2C_LIBRARY
 #include <Wire.h>
+#endif //USE_I2C_LIBRARY
 #endif
 #include "DS1307RTC.h"
 
-#define DS1307_CTRL_ID 0x68 
+#define DS1307_CTRL_ID (uint8_t)0x68
 
 DS1307RTC::DS1307RTC()
 {
+#ifdef USE_I2C_LIBRARY
+  I2c.begin();
+  I2c.timeOut((uint16_t) 5); //Set I2C timeout to 5ms
+#else //USE_I2C_LIBRARY
   Wire.begin();
+#endif //USE_I2C_LIBRARY
 }
   
 // PUBLIC FUNCTIONS
-time_t DS1307RTC::get()   // Aquire data from buffer and convert to time_t
+time_t DS1307RTC::get()   // Acquire data from buffer and convert to time_t
 {
   tmElements_t tm;
   if (read(tm) == false) return 0;
@@ -53,10 +64,29 @@ bool DS1307RTC::set(time_t t)
   return write(tm); 
 }
 
-// Aquire data from the RTC chip in BCD format
+// Acquire data from the RTC chip in BCD format
 bool DS1307RTC::read(tmElements_t &tm)
 {
   uint8_t sec;
+#ifdef USE_I2C_LIBRARY
+  if (I2c.write(DS1307_CTRL_ID,(uint8_t)0x00) != 0) {
+    exists = false;
+    return false;
+  }
+  exists = true;
+
+  // request the 7 data fields   (secs, min, hr, dow, date, mth, yr)
+  if (I2c.read(DS1307_CTRL_ID, (uint8_t)tmNbrFields)) return false;
+  if (I2c.available() < tmNbrFields) return false;
+  sec = I2c.receive();
+  tm.Second = bcd2dec(sec & 0x7f);
+  tm.Minute = bcd2dec(I2c.receive() );
+  tm.Hour =   bcd2dec(I2c.receive() & 0x3f);  // mask assumes 24hr clock
+  tm.Wday = bcd2dec(I2c.receive() );
+  tm.Day = bcd2dec(I2c.receive() );
+  tm.Month = bcd2dec(I2c.receive() );
+  tm.Year = y2kYearToTm((bcd2dec(I2c.receive())));
+#else //USE_I2C_LIBRARY
   Wire.beginTransmission(DS1307_CTRL_ID);
 #if ARDUINO >= 100  
   Wire.write((uint8_t)0x00); 
@@ -91,12 +121,31 @@ bool DS1307RTC::read(tmElements_t &tm)
   tm.Month = bcd2dec(Wire.receive() );
   tm.Year = y2kYearToTm((bcd2dec(Wire.receive())));
 #endif
+#endif //USE_I2C_LIBRARY
   if (sec & 0x80) return false; // clock is halted
   return true;
 }
 
 bool DS1307RTC::write(tmElements_t &tm)
 {
+#ifdef USE_I2C_LIBRARY
+  // To eliminate any potential race conditions,
+  // stop the clock before writing the values,
+  // then restart it after.
+  tm.Second |= 0x80;
+  if (I2c.write(DS1307_CTRL_ID,0x00,(uint8_t*)&tm,(uint8_t)0x08) != 0) {
+    exists = false;
+    return false;
+  }
+  exists = true;
+
+  // Now go back and set the seconds, starting the clock back up as a side effect
+  tm.Second &= ~0x80;
+  if (I2c.write(DS1307_CTRL_ID,(uint8_t)0x00,tm.Second) != 0) {
+    exists = false;
+    return false;
+  }
+#else //USE_I2C_LIBRARY
   // To eliminate any potential race conditions,
   // stop the clock before writing the values,
   // then restart it after.
@@ -139,12 +188,29 @@ bool DS1307RTC::write(tmElements_t &tm)
     exists = false;
     return false;
   }
+#endif //USE_I2C_LIBRARY
   exists = true;
   return true;
 }
 
 unsigned char DS1307RTC::isRunning()
 {
+#ifdef USE_I2C_LIBRARY
+  if(I2c.write(DS1307_CTRL_ID,(uint8_t)0x00)) {
+    exists = false;
+    return 2;
+  }
+  exists = true;
+
+  // Just fetch the seconds register and check the top bit
+  if(I2c.read(DS1307_CTRL_ID,(uint8_t)0x01)) {
+    exists = false;
+    return 2;
+  }
+  exists = true;
+
+  return !(I2c.receive() & 0x80);
+#else //USE_I2C_LIBRARY
   Wire.beginTransmission(DS1307_CTRL_ID);
 #if ARDUINO >= 100  
   Wire.write((uint8_t)0x00); 
@@ -160,12 +226,20 @@ unsigned char DS1307RTC::isRunning()
 #else
   return !(Wire.receive() & 0x80);
 #endif  
+#endif //USE_I2C_LIBRARY
 }
 
 void DS1307RTC::setCalibration(char calValue)
 {
   unsigned char calReg = abs(calValue) & 0x1f;
   if (calValue >= 0) calReg |= 0x20; // S bit is positive to speed up the clock
+#ifdef USE_I2C_LIBRARY
+  if(I2c.write(DS1307_CTRL_ID,(uint8_t)0x07,(uint8_t)calReg)) {
+    exists = false;
+    return;
+  }
+  exists = true;
+#else //USE_I2C_LIBRARY
   Wire.beginTransmission(DS1307_CTRL_ID);
 #if ARDUINO >= 100  
   Wire.write((uint8_t)0x07); // Point to calibration register
@@ -175,10 +249,26 @@ void DS1307RTC::setCalibration(char calValue)
   Wire.send(calReg);
 #endif
   Wire.endTransmission();  
+#endif //USE_I2C_LIBRARY
 }
 
 char DS1307RTC::getCalibration()
 {
+#ifdef USE_I2C_LIBRARY
+  if(I2c.write(DS1307_CTRL_ID,(uint8_t)0x07)) {
+    exists = false;
+    return 0x40;
+  }
+  exists = true;
+
+  if(I2c.read(DS1307_CTRL_ID,(uint8_t)0x01)) {
+    exists = false;
+    return 0x40;
+  }
+  exists = true;
+
+  unsigned char calReg = I2c.receive();
+#else //USE_I2C_LIBRARY
   Wire.beginTransmission(DS1307_CTRL_ID);
 #if ARDUINO >= 100  
   Wire.write((uint8_t)0x07); 
@@ -193,6 +283,7 @@ char DS1307RTC::getCalibration()
 #else
   unsigned char calReg = Wire.receive();
 #endif
+#endif //USE_I2C_LIBRARY
   char out = calReg & 0x1f;
   if (!(calReg & 0x20)) out = -out; // S bit clear means a negative value
   return out;
@@ -215,4 +306,3 @@ uint8_t DS1307RTC::bcd2dec(uint8_t num)
 bool DS1307RTC::exists = false;
 
 DS1307RTC RTC = DS1307RTC(); // create an instance for the user
-
